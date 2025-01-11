8392435ac6133ee72760be90e3171fe17bea05e0
theo shellman <theoshellman@gmail.com>
2025-01-11 16:29:32 -0500

    inital commit

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initalCard = [
+  { name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"},
+  { name: "Restaurant terrace", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"},
+  { name: "An outdoor cafe", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg" },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  { name: "Tunnel with morning light", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg" },
+  { name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" },
+];
+
+const profileEditButton = document.querySelector('.profile__edit-btn');
+
+const editProfileModal = document.querySelector('#edit-profile-modal');
+const closeProfilemodal = editProfileModal.querySelector('.modal__close-btn');
+
+function openModal() {
+  editProfileModal.classList.add('modal__opened');
+}
+  function closeModal() {
+    editProfileModal.classList.remove('modal__opened');
+
+}
+profileEditButton.addEventListener('click', openModal);
+closeProfilemodal.addEventListener('click', closeModal);
